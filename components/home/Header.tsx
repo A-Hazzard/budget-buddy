7b1074d34b3109be08f20ac3cc6dccfd44721c43
Aaron Hazzard <62683196+A-Hazzard@users.noreply.github.com>
2024-06-05 10:02:41 -0400

    Homepage responsive non-working navbar

--- components/home/Header.tsx
+++ components/home/Header.tsx
@@ -4,13 +4,14 @@ import Image from "next/image"
 import { ChevronDown } from 'lucide-react';
 import Button from "../Button";
 import { useMediaQuery } from "react-responsive";
+import Link from "next/link";
 
 export default function Header() {
     const isMobile = useMediaQuery({ maxWidth: 767 });
 
   return (
     <header className="pb-4 pl-3 pr-3 border-b flex items-end justify-between">
-        <div className="flex items-baseline">
+        <div className="flex items-end gap-3">
             <div className="w-24">
                 <Image 
                     src="/logo.svg"
@@ -20,7 +21,17 @@ export default function Header() {
                 />
             </div>
 
-        {isMobile ? (<ChevronDown className="h-7 w-7 ml-1 " />) : ''}
+            {isMobile ? (
+            <ChevronDown className="h-7 w-7 ml-1 " />
+            ) : (
+              <nav>
+                <ul className="flex gap-3">
+                    <li><Link href="#" className="font-primary cursor-pointer hover:underline hover:text-xl  transition-all">Learn to Budget</Link></li>
+                    <li><Link href="#" className="font-primary cursor-pointer hover:underline hover:text-xl  transition-all">Features</Link></li>
+                    <li><Link href="#" className="font-primary cursor-pointer hover:underline hover:text-xl  transition-all">About</Link></li>
+                </ul>
+              </nav>
+            )}
         </div>
         <Button text="Sign Up"/>
     </header>
