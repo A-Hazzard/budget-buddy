140e57d95b9819916591b9f14ce6244f741b3199
Aaron Hazzard <62683196+A-Hazzard@users.noreply.github.com>
2024-06-09 14:39:16 -0400

    Starlight Documentation

--- firebase.ts
+++ firebase.ts
@@ -6,13 +6,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBcpabocwOisr1Anu2QS-WpX96KMPy9Vv0',
-  authDomain: 'budget-buddy-94c0a.firebaseapp.com',
-  projectId: 'budget-buddy-94c0a',
-  storageBucket: 'budget-buddy-94c0a.appspot.com',
-  messagingSenderId: '445875554139',
-  appId: '1:445875554139:web:413f5151ab4b8f1cff7c7e',
-  measurementId: 'G-QJ04KE2C5P',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 let app;
 let auth: Auth;
---
+++ flaky-fireball/src/content/docs/home/home.md
@@ -0,0 +1,11 @@
+---
+title: Example Guide
+description: Intro.
+---
+
+# Budget Buddy Documentation
+
+Welcome to the Budget Buddy documentation site. Here you will find all the necessary information to get started with the project, including setup instructions and a detailed project report.
+
+![Image description](home.png)
+![Image description](dashboard.png)
